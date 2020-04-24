4622d6064e31f600700b7942ee4c54332bf31720
rahulnag <rahulnag625@gmail.com>
2020-04-25 00:59:11 +0530

    Updates

---
+++ precache-manifest.8830d485fdd221fcf3b8f733b1e925f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17671bf52b254630c128e7a688b61c4",
+    "url": "/ccl/index.html"
+  },
+  {
+    "revision": "7040c74386866fd9bd07",
+    "url": "/ccl/static/css/main.f4e754ad.chunk.css"
+  },
+  {
+    "revision": "76d75c99250dc461cb74",
+    "url": "/ccl/static/js/2.1db148cf.chunk.js"
+  },
+  {
+    "revision": "0441cdd28661ff2b6edbdca8139c3ecc",
+    "url": "/ccl/static/js/2.1db148cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7040c74386866fd9bd07",
+    "url": "/ccl/static/js/main.831f67ee.chunk.js"
+  },
+  {
+    "revision": "07fcf2aecfc6c3997c58",
+    "url": "/ccl/static/js/runtime-main.3236d6ae.js"
+  }
+]);
